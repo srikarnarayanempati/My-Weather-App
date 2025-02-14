df73834d7063f11d04739d76e57d5637cffcb1ea
srikar0110 <srikarnarayan116@gmail.com>
2025-02-14 12:07:07 +0530

    Fix Vite deployment path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'My-Weather-App',  // Change this to match your GitHub repo name
+
 })
